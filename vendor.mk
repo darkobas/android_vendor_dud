8be4c00168df2a7f79736bf1069facb84c7f3809
darkobas <darkobas@gmail.com>
2015-09-11 13:36:03 +0200

    update version

--- vendor.mk
+++ vendor.mk
@@ -3,9 +3,9 @@ export VENDOR := dud
 
 # DUD version info
 DUD_VERSION_MAJOR := 2
-DUD_VERSION_MINOR := 3
-DUD_VERSION_MAINTENANCE := .9.2
+DUD_VERSION_MINOR := 4
+DUD_VERSION_MAINTENANCE := .3
 DUD_TYPE := darkobas
-DUD_VERSION_TAG := BETA
+DUD_VERSION_TAG := STABLE
 
 include vendor/dud/configs/version.mk
